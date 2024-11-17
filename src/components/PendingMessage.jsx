51f06d4811f3cc855b5f176d5afd0c68f12037b3
unknown <valentimega@gmail.com>
2024-11-17 18:03:46 -0300

    PendigMessage and ContactForm

--- src/components/PendingMessage.jsx
+++ src/components/PendingMessage.jsx
@@ -1,11 +1,96 @@
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import IconGmail from "./icons/IconGmail";
+import IconMessageOk from "./icons/IconMessageOk";
 
 const PendingMessage = () => {
-    return (
+  const [pending, setPending] = useState([]);
+  const [error, setError] = useState(null);
+  const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
-        <h1>Mensajes Pendientes</h1>
-    )
-}
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/api/contacts`, {
+          withCredentials: true,
+        });
+        const pendingMessages = response.data.filter(
+          (message) => !message.status
+        );
+        setPending(pendingMessages);
+      } catch (error) {
+        console.error("Error al obtener los mensajes: ", error);
+        setError("Hubo un problema al cargar los mensajes");
+      }
+    };
+    fetchMessage();
+  }, []);
 
-export default PendingMessage
+  const handleMarkAsResponded = async (id) => {
+    try {
+      await axios.put(
+        `${apiUrl}/api/contact/${id}`,
+        { status: true },
+        { withCredentials: true }
+      );
+
+      setPending((prev) => prev.filter((message) => message._id !== id));
+    } catch (error) {
+      console.error("Error al marcar el mensaje como respondido: ", error);
+    }
+  };
+
+  if (error) {
+    return <p className="text-center text-red-500">{error}</p>;
+  }
+
+  return (
+    <div className="max-2-4wl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Mensajes Pendientes
+      </h1>
+      {error && <p className="text-red-500 mb-4">{error}</p>}
+      {pending.length === 0 ? (
+        <p className="text-center text-green-500 font-semibold">
+          ¡Estás al día con los mensajes!
+        </p>
+      ) : (
+        <ul className="divide-y divide-gray-200">
+          {pending.map((x) => (
+            <li key={x.id} className="py-4 flex items-center justify-between">
+              <div>
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {x.name}
+                </h2>
+                <p className="text-gray-600">{x.email}</p>
+                <p className="text-gray-500 mt-1">{x.message}</p>
+              </div>
+              <div className="py-4 flex items-center justify-between">
+                <a
+                  href={`https://mail.google.com/mail/?view=cm&fs=1&to=${
+                    x.email
+                  }&su=Respuesta%20a%20tu%20mensaje&body=Hola%20${encodeURIComponent(
+                    x.name
+                  )},%0A%0AGracias%20por%20contactarte%20con%20nosotros.%20A%20continuación%20te%20respondemos:%0A%0A`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=" text-red-500 hover:text-red-900 font-bold py-2 px-4 rounded hover:bg-gray-200"
+                >
+                  <IconGmail />
+                </a>
+                <button
+                  onClick={() => handleMarkAsResponded(x._id)}
+                  className=" text-green-500 hover:text-green-900 font-bold py-2 px-4 rounded hover:bg-gray-200"
+                >
+                  <IconMessageOk />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PendingMessage;
--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -1,7 +1,140 @@
-function Contact(){
-    return(
-        <h1>Contact Form</h1>
-    )
-}
+import React, { useState } from "react";
+import axios from "axios";
 
-export default Contact
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [status, setStatus] = useState(null);
+  const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message) {
+      setStatus({
+        succes: false,
+        message: "Todos los campos son obligatorios",
+      });
+      setTimeout(() => {
+        setStatus("")
+      
+      }, 1500)
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${apiUrl}/api/addcontacts`, formData, {
+        withCredentials: true,
+      });
+
+      setStatus({
+        succes:false,
+        message:"Gracias por comunicarte con nosotros"})
+      setFormData({ name: "", email: "", message: "" });
+      setTimeout(() => {
+        setStatus("")
+      
+      }, 1500)
+    } catch (error) {
+      console.error("Error al nviar el formulario: ", error);
+      setStatus({
+        succes: false,
+        message: error.response?.data?.error || "Error al enviar el formulario",
+      });
+      setTimeout(() => {
+        setStatus("")
+      
+      }, 1500)
+    }
+  };
+
+  return (
+    <div className="max-w-xl mx-auto p-8 ">
+      
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white  px-8 pt-6 pb-8"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Ingresa tu nombre"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Correo electrónico
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Ingresa tu correo"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="message"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Mensaje
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            rows="4"
+            placeholder="Escribe tu mensaje"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Enviar
+        </button>
+      </form>
+      {status && (
+        <div
+          className={`mt-4 p-4 rounded ${
+            status.success ? "bg-green-100 text-green-700" : "bg-red-100 text-red-700"
+          }`}
+        >
+          {status.message}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Contact;
